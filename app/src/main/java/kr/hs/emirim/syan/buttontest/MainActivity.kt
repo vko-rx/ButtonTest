bab2edf7f2df82b98ec7fa63085a864ddf3bfef7
vko-rx <amhdksalsgnl@gmail.com>
2023-04-13 20:04:43 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/syan/buttontest/MainActivity.kt
@@ -0,0 +1,15 @@
+package kr.hs.emirim.syan.buttontest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        var btn1: Button
+//        btn1 = findViewById(R.id.btn1)
+        setContentView(R.layout.layout3)
+    }
+}
